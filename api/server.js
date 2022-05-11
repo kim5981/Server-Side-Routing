35faf96ac54db3303c702e9310ef98fa9ec6cbc2
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-10 19:36:34 -0700

    server set up

--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
+const express = require("express")
+const server = express();
+
 // require your posts router and connect it here
+server.use(express.json());
+
+// plug in server
+server.use("*", (req, res) => {
+    res.status(404).json({
+        message: "um you did smth wrong.."
+    })
+})
+
+module.exports = server;
