3df35faa91008c90fe2dd3910a2a6d300c839f16
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-10 21:21:55 -0700

    post wired

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -50,18 +50,38 @@ router.get("/:id", async (req, res) => {
 
 
 //* POST /api/posts
-router.get("/", (req, res) => {
-    
+router.post("/", (req, res) => {
+    const { title, contents } = req.body
+    if( !title || !contents ) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Posts.insert({ title, contents })
+        .then( ({ id }) => {
+            return Posts.findById(id)
+        })
+        .then(post => {
+            res.status(201).json(post)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "There was an error while saving the post to the database",
+                err: err.message,
+                stack: err.stack
+            })
+        })
+    }
 })
 
 //* PUT /api/posts/:id
-router.get("/", (req, res) => {
+router.put("/", (req, res) => {
     
 })
 
 
 //* DELETE /api/posts/:id
-router.get("/", (req, res) => {
+router.delete("/", (req, res) => {
     
 })
 
